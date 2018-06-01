777c6b90312a3d19c435b55ad8710c5ec4c09595
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-05-31 21:31:14 -0400

    added User login page

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, login_page
 urlpatterns = [
     path('', home_page),
     path('admin/', admin.site.urls),
     path('about/', about_page),
+    path('login/', login_page),
     path('contact/', contact_page)
 ]
