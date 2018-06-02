7cb4e618f73d03ff3ddbb4ff49f86d18ea037819
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-06-02 19:54:16 -0400

    detail page for product

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProducDetailView, product_detail_view
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -32,7 +32,9 @@ urlpatterns = [
     url(r'^register/$', register_page),
     url(r'^admin/', admin.site.urls),
     url(r'^products/$', ProductListView.as_view()),
-    url(r'^products-fbv/$', product_list_view)
+    url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d+)/$', ProducDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view)
 ]
 
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic.list import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 # Create your views here.
@@ -22,3 +22,26 @@ def product_list_view(request):
     }
 
     return render(request, "products/list.html", context)
+
+
+
+class ProducDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+
+        context = super(ProducDetailView, self).get_context_data(*args, **kwargs)
+
+        return context
+    
+
+def product_detail_view(request, pk=None, *args,**kwargs):
+
+    #instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product,pk=pk)
+    context = {
+        'object': instance
+    }
+
+    return render(request, "products/detail.html", context)
