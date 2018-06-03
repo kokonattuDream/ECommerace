e359ad8fb4f6e9973be17e7e74129bfb688d6b63
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-06-03 10:26:24 -0400

    customized the model manger

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Product
 # Create your views here.
@@ -14,6 +15,12 @@ class ProductListView(ListView):
 
         return context
 
+    def get_queryset(self, *args, **kwargs):
+
+        request = self.request
+        
+        return Product.objects.all()
+
 def product_list_view(request):
 
     queryset = Product.objects.all()
@@ -26,7 +33,7 @@ def product_list_view(request):
 
 
 class ProducDetailView(DetailView):
-    queryset = Product.objects.all()
+
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -35,15 +42,24 @@ class ProducDetailView(DetailView):
 
         return context
     
+    def get_object(self, *args, **kwargs):
 
+        request = self.request
+        pk = self.kwargs.get('pk')
+
+        instance = Product.objects.get_by_id(pk)
+
+        if instance is None:
+            raise Http404("product doesn't exist")
+
+        return instance
+    
 def product_detail_view(request, pk=None, *args,**kwargs):
 
-    qs = Product.objects.filter(id=pk)
-    
-    if qs.exists() and qs.count() == 1:
-        instance = qs.first()
-    else:
-        raise Http404("Product doesn't exist")
+    instance = Product.objects.get_by_id(pk)
+
+    if instance is None:
+        raise Http404("product doesn't exist")
 
     context = {
         'object': instance
